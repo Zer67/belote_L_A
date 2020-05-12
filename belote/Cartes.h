c6a6afd844a97d7f88e41bfce235e9f7c580a51f
Karakala0 <64852376+Karakala0@users.noreply.github.com>
2020-05-12 18:45:40 +0200

    Add files via upload

---
+++ belote/Cartes.h
@@ -0,0 +1,18 @@
+//
+//  Cartes.h
+//  Belote
+//
+//  Created by Michel HESSE on 08/05/2020.
+//  Copyright © 2020 Léonard HESSE. All rights reserved.
+//
+
+#ifndef Cartes_h
+#define Cartes_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+void DistributeCards(char** North, char** South, char** East, char** West);
+#endif /* Cartes_h */
