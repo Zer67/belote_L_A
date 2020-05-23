db5920a03251aa58add53cd8f5966a442a8edeb1
Karakala0 <64852376+Karakala0@users.noreply.github.com>
2020-05-23 14:18:10 +0200

    Update Cartes.c

--- belote/Cartes.c
+++ belote/Cartes.c
@@ -10,7 +10,7 @@
 
 
 
-void DistributeCards (Cards* North, Cards* South, Cards* East, Cards* West) {
+void DistributeCards (Player* North, Player* South, Player* East, Player* West) {
     srand(time(0));
     
     Cards Distribution[32] = {{"Hea_7",0,0}, {"Hea_8",1,0}, {"Hea_9",2,0}, {"Hea_Ja",3,2}, {"Hea_Qu",4,3}, {"Hea_Ki",5,4}, {"Hea_10",6,10}, {"Hea_1",7,11},
@@ -23,122 +23,72 @@ void DistributeCards (Cards* North, Cards* South, Cards* East, Cards* West) {
     //Heart :[0;7]   Club :[8;15]   Diamond :[16;23]   Spade :[23;31]
     
 
-       int l = 0;
+    int l = 0;
     
-/*
-************************************************************************************************************************************************
- ************************************************************Initializing North's hand***********************************************************
-  *************************************************************************************************************************************************/
+    l = FullingHand(North, l, Distribution); //Initializing North's Hand
+    l = FullingHand(South, l, Distribution); //Initializing South's Hand
+    l = FullingHand(East, l, Distribution); //Initializing East's Hand
     
-    for (int i = 0; i < 8; i++) {
-        int a = rand()%(31-l);
-        North[i] = Distribution[a] ;
-        
-        for (int u = a; u < 31-l; u++) {
-            Distribution[u] = Distribution[u+1];
-        }
-        l++;
+    for (int i = 0; i <8; i++){
+        West->hand[i] = Distribution[i]; //Initializing West's Hand
     }
-    
-    SorteDeck(North);
-    
-    printf("\nNorth : ");
-      for (int i = 0; i <8; i++) {
-          printf("%s, ", North[i].color);
-      }
-   
-/*
-************************************************************************************************************************************************
-************************************************************Initializing East's hand***********************************************************
- ************************************************************************************************************************************************
-*/
-
-    for (int i = 0; i < 8; i++) {
-        int a = rand()%(31-l);
-        East[i] = Distribution[a];
-        
-        for (int u = a; u < 31-l; u++) {
-            Distribution[u] = Distribution[u+1];
-        }
-
-        l++;
-    }
-    
-     SorteDeck(East);
-    
-     printf("\nEast : ");
-       for (int i = 0; i <8; i++) {
-           printf("%s, ", East[i].color);
-       }
-    
-
-/*
-************************************************************************************************************************************************
-************************************************************Initializing South's hand***********************************************************
- ************************************************************************************************************************************************
-*/
-    
-    for (int i = 0; i < 8; i++) {
-        int a = rand()%(31-l);
-        South[i] = Distribution[a];
-        
-        for (int u = a; u < 31-l; u++) {
-            Distribution[u] = Distribution[u+1];
-        }
-        l++;
-    }
-    
-     SorteDeck(South);
-    
-     printf("\nSouth : ");
-       for (int i = 0; i <8; i++) {
-           printf("%s, ", South[i].color);
-       }
-    
-
-    
-
-/*
-************************************************************************************************************************************************
-************************************************************Initializing West's hand***********************************************************
- ************************************************************************************************************************************************
-*/
-    
-    for (int i = 0; i <= 31-l; i++) {
-        West[i] = Distribution[i];
-
-    }
- 
-     SorteDeck(West);
+    SorteDeck(West);
 }
 
 
 
-void SorteDeck(Cards* Player) {
-    int l= 0, max_index;
-    Cards max;
+
+int FullingHand(Player* player, int l, Cards* Distribution) {
+    int a;
+    
+    for (int i = 0; i <8; i++) {
+        a = rand()%(31-l);
+        player->hand[i] = Distribution[a];
+    
+    
+        for (int u = a; u < 31-l; u++) {
+            Distribution[u] = Distribution[u+1];
+        }
+    l++;
+    }
+    
+    SorteDeck(player);
+
+    
+    return l;
+}
+
+
+
+void SorteDeck(Player* Player) {  // I use the Selection sort 
+    int l= 0, max_index;          
+    Cards max;                    
 
      do{
-        max = Player[0];
+         max = Player->hand[0];
          max_index = 0;
          
          for (int i = 1; i < 8-l; i++){
-             if (Player[i].color[0] > max.color[0]){
-                     max = Player[i];
+             if (Player->hand[i].color[0] > max.color[0]){  //Comparing the first character of the field "color"
+                     max = Player->hand[i];                 //Sorting the hands of each player by gathering the Cards according to their colors
                      max_index = i;
-             }else if (Player[i].color[0] == max.color[0] && Player[i].power > max.power) {
-                     max = Player[i];
+             }else if (Player->hand[i].color[0] == max.color[0] && Player->hand[i].power > max.power) { .
+                     max = Player->hand[i];                 //and then in ascending order according to the field "power" of the struct Cards
                      max_index = i;
             }
         }
             
          for (int u = max_index; u < 7-l; u++) {
-             Player[u] = Player[u+1];
+             Player->hand[u] = Player->hand[u+1];
          }
-         Player[7-l] = max;
+         Player->hand[7-l] = max;
          l++;
          
      }while(l<7);
-
+    
+    printf("\n%s : ", Player->name);
+    for (int i = 0; i<8; i++) {
+        printf("%s, ", Player->hand[i].color);
+    }
+    
 }
-
