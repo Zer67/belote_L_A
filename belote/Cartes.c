fb20620d011c0add31e0bb32440d684ccf4b6349
Karakala0 <64852376+Karakala0@users.noreply.github.com>
2020-06-04 15:28:53 +0200

    Update Cartes.c

--- belote/Cartes.c
+++ belote/Cartes.c
@@ -7,8 +7,13 @@
 //
 
 #include "Cartes.h"
+#include "menu.h"
 
 
+/*
+ *******************************************************************************************************************
+ ****************************************** Distribution of the Cards **********************************************
+ *******************************************************************************************************************/
 
 void DistributeCards (Player* North, Player* South, Player* East, Player* West) {
     srand(time(0));
@@ -32,9 +37,8 @@ void DistributeCards (Player* North, Player* South, Player* East, Player* West) 
     for (int i = 0; i <8; i++){
         West->hand[i] = Distribution[i]; //Initializing West's Hand
     }
-    SorteDeck(West);
+    SorteHand(West);
 }
-
 
 
 
@@ -52,7 +56,7 @@ int FullingHand(Player* player, int l, Cards* Distribution) {
     l++;
     }
 
-    SorteDeck(player);
+    SorteHand(player);
 
 
     return l;
@@ -60,7 +64,7 @@ int FullingHand(Player* player, int l, Cards* Distribution) {
 
 
 
-void SorteDeck(Player* Player) {  // I use the Selection sort
+void SorteHand(Player* Player) {  // I use the Selection sort
     int l= 0, max_index;
     Cards max;
 
@@ -91,4 +95,147 @@ void SorteDeck(Player* Player) {  // I use the Selection sort
         printf("%s, ", Player->hand[i].color);
     }
 
+}
+
+void ChangeScore(char trump_color, Player* Player){
+
+    //Changing the score of the Cards of North's hand.
+    for (int i = 0; i < 8; i++){
+        if(Player->hand[i].color[0] == trump_color){
+            Player->hand[i].power += 20;
+            
+            switch (Player->hand[i].color[5]) {
+                case '9':
+                    Player->hand[i].point = 14;
+                    Player->hand[i].power = 28;
+                    break;
+                case 'J':
+                    Player->hand[i].point = 20;
+                    Player->hand[i].power =29;
+                default:
+                    break;
+            }
+        }
+    }
+    
+}
+
+/** this function shift an array of four players from a specified number.
+ * @param playerArray - the array of player we want to shift
+ * @param shift - the number of shift we want to do
+ * @return the array of player shifted
+ */
+Player* shiftPlayers(Player* playerArray, int shift, int size){
+    if((playerArray != NULL)||(size!=4)){
+        for(int j=0; j<(shift%size);j++){   /* here we use the modulo to avoid extra shifts which will be useless */
+            Player temp = playerArray[0];   /* here is a temporal variable which is useful to stock the first player of the array */
+            for(int i = 0; i<size-1; i++){
+                playerArray[i] = playerArray[i+1];
+            }
+            playerArray[size-1] = temp;
+        }
+    }
+    return playerArray;
+}
+
+/** this function allows us to know where a given player is in an array of player.
+ * @param playerArray - the array where we're looking for the player
+ * @param player - the player we're looking for
+ * @return the position of the player in the array
+ */
+int FindPosition(Player* playerArray,char* player){
+    int index = 0;
+    while((strcmp(playerArray[index].name, player)!=0) &&(index<4)){
+        index++;
+    }
+    return index;
+}
+
+/*
+*******************************************************************************************************************
+*********************************************** Inside the game ***************************************************
+*******************************************************************************************************************/
+
+
+void Game_of_South(Player* South, int turn, int Card_in_theTrick, TricksStats* TheTrick, char trump_color) {
+    char readString[5];
+    int ind = -1;
+    
+    
+    printf("\nYour Hand : ");
+    for (int i = 0; i <= turn; i++) {
+          printf("%s, ", South->hand[i].color);
+      }
+
+    do {
+         printf("\nType the index of the Card you want to play (from 1 to %i) : ", turn+1);
+        scanf("%s",readString);
+    } while ((sscanf(readString, "%d", &ind) == EOF) || (ind <= 0) || (ind > turn+1));
+    
+    ind--;
+    printf("\nSouth : %s", South->hand[ind].color);
+    
+    TheTrick->CardsOfTheTrick[0] = South->hand[ind];
+    strcpy(TheTrick->NameOfWinner, "South");
+    TheTrick->TeamWinningNumber = South->TeamNumber;
+    
+    for (int u = ind; u < turn; u++) {
+        South->hand[u] = South->hand[u+1];
+    }
+    turn--;
+    
+
+}
+
+
+
+
+
+
+
+void printTheTrick(TricksStats* TheTrick, Player* players, int Card_in_theTrick) {
+    int i = 0;
+    Boolean Found = FALSE;
+    
+    printf("\n\t\tNorth");
+    while(!Found) {
+        if (strcmp(players[i].name, "North")==0){
+            printf("\n\t\t%s", TheTrick->CardsOfTheTrick[i].color);
+            Found = TRUE;
+        }
+        i++;
+    }
+    
+    Found = FALSE;
+    i = 0;
+    printf("\nWest");
+       while(!Found) {
+           if (strcmp(players[i].name, "West")==0){
+               printf("\t%s", TheTrick->CardsOfTheTrick[i].color);
+               Found = TRUE;
+           }
+           i++;
+       }
+    
+    Found = FALSE;
+    i = 0;
+       while(!Found) {
+           if (strcmp(players[i].name, "East")==0){
+               printf("   %s", TheTrick->CardsOfTheTrick[i].color);
+               Found = TRUE;
+           }
+           i++;
+       }
+    printf("\tEast");
+    
+    Found = FALSE;
+    i = 0;
+       while(!Found) {
+           if (strcmp(players[i].name, "South")==0){
+               printf("\n\t\t%s", TheTrick->CardsOfTheTrick[i].color);
+               Found = TRUE;
+           }
+           i++;
+       }
+    printf("\n\t\tSouth");
 }
