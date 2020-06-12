1bbc0909e27ed909160b8238741926d58d7082b4
Karakala0 <64852376+Karakala0@users.noreply.github.com>
2020-06-12 10:55:55 +0200

    Update AI.c

--- belote/AI.c
+++ belote/AI.c
@@ -170,45 +170,29 @@ void Game_of_AI(Player* Player, int turn, int Card_in_theTrick, TricksStats* The
     
 if (Card_in_theTrick == 0){                                   //Checks if the Player plays the first card of the trick.
 
-        while (HasPlayed == FALSE && i<= turn) {                  //First, plays a non-trump big Card, if possible
-           if(Player->hand[i].color[0] != trump_color && Player->hand[i].power > 5){
-                TheTrick->CardsOfTheTrick[0] = Player->hand[i];
-                HasPlayed = TRUE;                                 //if the conditions are verified, it means he has played,
-                i--;                                              //then the function stops.
-            }
+        while (Player->hand[i].color[0] != trump_color && Player->hand[i].power < 5 && i<= turn) {  //First, plays a non-trump big Card, if possible
             i++;
         }
 
-        if(HasPlayed == FALSE){                                    //If not played before, the Player plays a non-trump small Card.
+        if(i > turn){                                    //If not played before, the Player plays a non-trump small Card.
             i = 0;
-            while (HasPlayed == FALSE && i<= turn){
-                if(Player->hand[i].color[0] != trump_color && Player->hand[i].power < 3){
-                    TheTrick->CardsOfTheTrick[0] = Player->hand[i];
-                    HasPlayed = TRUE;
-                    i--;
-                }
+            while (Player->hand[i].color[0] != trump_color && Player->hand[i].power > 3 && i <= turn+1){
                 i++;
             }
-            if (HasPlayed == FALSE){
+            if (i > turn){
                 i = 0;
-                while (HasPlayed == FALSE && i<=turn){       //If not played before, the Player plays a trump.
-                   if(Player->hand[i].color[0] == trump_color){
-                       TheTrick->CardsOfTheTrick[0] = Player->hand[i];
-                       HasPlayed = TRUE;
-                       i--;
-                   }
+                while (Player->hand[i].color[0] == trump_color && i<=turn){       //If not played before, the Player plays a trump.
                    i++;
                 }
-                if (HasPlayed == FALSE){                    //If the player can't respect all the previous condition,
+                
+                if (i > turn){                    //If the player can't respect all the previous condition,
                     i = 0;                          //he plays the first card of his hand.
                     TheTrick->CardsOfTheTrick[0] = Player->hand[i];
                 }
             }
 
         }
-        strcpy(TheTrick->NameOfWinner, Player->name);
-        TheTrick->TeamWinningNumber = Player->TeamNumber;
-        TheTrick->indexWinningCards = Card_in_theTrick;
+    TheTrick->CardsOfTheTrick[0] = Player->hand[i];
         for (int u = i; u <= turn; u++) {
               Player->hand[u] = Player->hand[u+1];
         }
@@ -218,7 +202,7 @@ if (Card_in_theTrick == 0){                                   //Checks if the Pl
 else {                                                                      //If the player isn't the first to play
 
         int o = 0;
-        Cards FirstCardPlayed = TheTrick->CardsOfTheTrick[0]; //It is the first card played.
+        char FirstCardColor = TheTrick->CardsOfTheTrick[0].color[0]; //It is the first card played.
         Cards WinningCard = TheTrick->CardsOfTheTrick[TheTrick->indexWinningCards];//It is the card that wins the trick
         
         for (int u = 0; u <= turn; u++){
@@ -228,13 +212,13 @@ else {                                                                      //If
         }
         
         for (int u = 0; u <= turn; u++){
-            if (Player->hand[u].color[0] != trump_color && Player->hand[u].point > Player->hand[max].point){
+            if (Player->hand[u].color[0] != trump_color && Player->hand[u].power > Player->hand[max].power){
                 max = u;                                //max takes the index of the non-trump card with the most value in the player's hand
             }
         }
 
 
-        while (Player->hand[i].color[0] != FirstCardPlayed.color[0] && i <= turn+1){
+        while (Player->hand[i].color[0] != FirstCardColor && i <= turn+1){
             i++;
         }
         // If i < turn+1, it means the player has at least one card of the trick's color,
@@ -255,39 +239,14 @@ else {                                                                      //If
                 }
             //Player->hand[o] is the biggest card of the trick's color in the player's hand
             
-            if (FirstCardPlayed.color[0] != trump_color) {
-                    if (Player->hand[o].power > WinningCard.power){                     //The player wins the trick
-                        TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];
-                        strcpy(TheTrick->NameOfWinner, Player->name);
-                        TheTrick->TeamWinningNumber = Player->TeamNumber;
-                        TheTrick->indexWinningCards = Card_in_theTrick;
-
-                    }else if (TheTrick->TeamWinningNumber == Player->TeamNumber) {      //The player plays a big card since is team wins the trick
-                        TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];
-
-                    } else {                                                            //The player plays the smallest card of the trick's color
-                        o = i;
-                        TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];
-                    }
-            }
-            else {                                                                      //If the first card is a trump
-                if (Player->hand[o].power > WinningCard.power){                         //If the player can win the trick
-                    TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];
-                    strcpy(TheTrick->NameOfWinner, Player->name);
-                    TheTrick->TeamWinningNumber = Player->TeamNumber;
-                    TheTrick->indexWinningCards = Card_in_theTrick;
-            
-                }else{                                                                  //If the player can't win the trick
+                if (Player->hand[o].power < WinningCard.power && TheTrick->TeamWinningNumber != Player->TeamNumber){
+                //If the player can't win the trick and if the player's team doesn't win the trick
                     o = i;
-                    TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];
-              
                 }
-            }
+            
         }else {                                              //If the player has not the trick's color
-                    HasPlayed = FALSE;
                     o = 0; i =0;
             
-
                     while (Player->hand[i].color[0] != trump_color && i <= turn+1){ //We check if the player has still some trump
                         i++;
                     }
@@ -295,15 +254,13 @@ else {                                                                      //If
 
                     if(TheTrick->TeamWinningNumber == Player->TeamNumber && Card_in_theTrick == 3){
                         // if the winning card is a trump, if the player's team wins the trick and if the player is the last to play
-                        o = max;
-                        TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];   //He plays his biggest non-trump card
-
+                        o = max;                                                        //He plays the biggest non-trump card
 
                     }else if (WinningCard.color[0] == trump_color && TheTrick->TeamWinningNumber != Player->TeamNumber && i <= turn){
                         //If the winning card is a trump, if the player's team looses the trick and if the player has a trump
                         o = i;
                         while (HasPlayed==FALSE && o <= turn+1){       //We search for a trump card who wins the trick
-                            if (Player->hand[o].color[0] == trump_color && Player->hand[o].power > WinningCard.power) {
+                            if (Player->hand[o].power > WinningCard.power) {
                                 HasPlayed = TRUE;
                                 o--;
                             }
@@ -311,31 +268,20 @@ else {                                                                      //If
                         }
                         
                         
-                        if (HasPlayed == TRUE){         //If he can win the trick
-                            TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];  //He plays the card that wins the trick
-                            strcpy(TheTrick->NameOfWinner, Player->name);
-                            TheTrick->TeamWinningNumber = Player->TeamNumber;
-                            TheTrick->indexWinningCards = Card_in_theTrick;
-
-                        }else {                         //If he can't win the trick
-                            o = min;                    //He plays his smallest card
-                            TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];
+                        if (o > turn){
+                            o = min;
                         }
+
                         
                         
                     }else if (WinningCard.color[0] != trump_color && i <= turn) {        //If the winning card is not a trump and if he has a trump
-                   
-                        o = i;
-                        TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];      //he plays the smallest trump card
-                        strcpy(TheTrick->NameOfWinner, Player->name);
-                        TheTrick->TeamWinningNumber = Player->TeamNumber;
-                        TheTrick->indexWinningCards = Card_in_theTrick;
+                        o = i;                                                          //he plays the smallest trump card
                         
                     }else{                                                               //If he has no trump card
-                        o = min;
-                        TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];  //he plays the smallest card of his hand
+                        o = min;                                                    //he plays the smallest card of his hand
                     }
             }
+    TheTrick->CardsOfTheTrick[Card_in_theTrick] = Player->hand[o];
         for (int u = o; u <= turn; u++) {
               Player->hand[u] = Player->hand[u+1];
         }
